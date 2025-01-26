60897ad5a2f4059e19ca6448971805c840764520
sambou <sarrsambou03@gmail.com>
2025-01-26 14:45:43 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>site de vente de materiel Electromenager</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <meta name="google-adsense-account" content="ca-pub-3371837432014834">
+  <!-- Favicons 
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: iPortfolio
+  * Updated: Jan 09 2024 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3371837432014834"
+     crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+ 
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+            <img src="assets/img/portfolio/logo.png" alt="" class="img-fluid rounded-circle">
+       
+        <h1 class="text-light"><a href="index.html">Lebouto Services</a></h1>
+        
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>home</span></a></li>
+          <li><a href="#portfolio" class="nav-link scrollto active"><i class="bx bx-fridge"></i> <span>Électroménager</span></a></li>
+          <li><a href="#portfolio" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Solaire</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center" style="background-image: url('assets/img/portfolio/f1.png');">
+    <div class="hero-container" data-aos="fade-in">
+      <h1 class="display-3">Bienvenue sur LeboutoService</h1>
+      <p> <span class="typed" data-typed-items="Bienvenue chez Lebouto Services, la boutique où la qualité rencontre la satisfaction."></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+  <section id="portfolio" class="portfolio section-bg">
+  <div class="container">
+    <div class="section-title">
+      <h2>Nos produits</h2>
+      <p>
+        Explorez notre collection d'électroménager qui allie qualité, design et performance. Trouvez des produits qui transforment votre quotidien.
+      </p>
+    </div>
+
+    <div class="row" data-aos="fade-up">
+      <div class="col-lg-12 d-flex justify-content-center">
+        <ul id="portfolio-flters" class="list-unstyled d-flex gap-3">
+          <li data-filter="*" class="filter-active">Tous</li>
+          <li data-filter=".filter-cuisine">Cuisine</li>
+          <li data-filter=".filter-entretien">Entretien</li>
+          <li data-filter=".filter-divers">Autres</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+      <!-- Produit 1 -->
+      <div class="col-lg-4 col-md-6 portfolio-item filter-cuisine">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/cui.jpeg" class="img-fluid" alt="Réfrigérateur">
+          <div class="portfolio-links">
+            <a href="assets/img/portfolio/cui.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Réfrigérateur">
+              <i class="bx bx-plus"></i> 
+            </a>
+            <a href="https://wa.me/221773881690?text=Je%20suis%20intéressé%20par%20le%20réfrigérateur.">
+              <i class="bx bx-cart"></i> 
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Produit 2 -->
+      <div class="col-lg-4 col-md-6 portfolio-item filter-entretien">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/cui.jpeg" class="img-fluid" alt="Machine à laver">
+          <div class="portfolio-links">
+            <a href="assets/img/portfolio/cui.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Machine à laver">
+              <i class="bx bx-plus"></i> 
+            </a>
+            <a href="https://wa.me/221773881690?text=Je%20souhaite%20acheter%20la%20machine%20à%20laver.">
+              <i class="bx bx-cart"></i> 
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-entretien">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/cui.jpeg" class="img-fluid" alt="Machine à laver">
+          <div class="portfolio-links">
+            <a href="assets/img/portfolio/cui.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Machine à laver">
+              <i class="bx bx-plus"></i> 
+            </a>
+            <a href="https://wa.me/221773881690?text=Je%20souhaite%20acheter%20la%20machine%20à%20laver.">
+              <i class="bx bx-cart"></i> 
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-entretien">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/cui.jpeg" class="img-fluid" alt="Machine à laver">
+          <div class="portfolio-links">
+            <a href="assets/img/portfolio/cui.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Machine à laver">
+              <i class="bx bx-plus"></i> 
+            </a>
+            <a href="https://wa.me/221773881690?text=Je%20souhaite%20acheter%20la%20machine%20à%20laver.">
+              <i class="bx bx-cart"></i> 
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-entretien">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/cui.jpeg" class="img-fluid" alt="Machine à laver">
+          <div class="portfolio-links">
+            <a href="assets/img/portfolio/cui.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Machine à laver">
+              <i class="bx bx-plus"></i> 
+            </a>
+            <a href="https://wa.me/221773881690?text=Je%20souhaite%20acheter%20la%20machine%20à%20laver.">
+              <i class="bx bx-cart"></i> 
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- Produit 3 -->
+      <div class="col-lg-4 col-md-6 portfolio-item filter-divers">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/cui.jpeg" class="img-fluid" alt="Four à micro-ondes">
+          <div class="portfolio-links">
+            <a href="assets/img/portfolio/cui.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Four à micro-ondes">
+              <i class="bx bx-plus"></i> 
+            </a>
+            <a href="https://wa.me/221773881690?text=Je%20suis%20intéressé%20par%20le%20four%20à%20micro-ondes.">
+              <i class="bx bx-cart"></i> 
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</main>
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>2024</span></strong>
+        <div class="social-links mt-3 text-center">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        </div>
+      </div>
+      
+      <div class="credits">
+            Designed by <a href="https://wa.me/221772476160?text=Salut%20!%20Je%20suis%20intéressé%20par%20votre%20travail%20e%20(%20creation%20de%20site%20web)">Sambou</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
